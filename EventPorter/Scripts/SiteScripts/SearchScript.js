304ea7367d0256e5c8ddf925d437768eb0bb2d4d
10366827 <10366827@mydbs.ie>
2017-09-10 21:28:46 +0100

    Initial Usable Search Bar - Index Page

---
+++ EventPorter/Scripts/SiteScripts/SearchScript.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function (e) {
+    $('.search-panel .dropdown-menu').find('a').click(function (e) {
+        e.preventDefault();
+        var param = $(this).attr("href").replace("#", "");
+        var concept = $(this).text();
+        $('.search-panel span#search_concept').text(concept);
+        $('.input-group #search_param').val(param);
+    });
+
+    $('#search_button').click(function (e) {
+        var search_filter = $('#search_param').val();
+        var search_text = $('#search_input').val();
+        alert("Filter: " + search_filter + " User input: " + search_text);
+    });
+});
