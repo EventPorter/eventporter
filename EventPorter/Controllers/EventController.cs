7b5e8b02d46040568a736e8bffeb08e0476c3ea3
10366827 <10366827@mydbs.ie>
2017-09-10 15:53:17 +0100

    Event Stuff

---
+++ EventPorter/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventPorter.Controllers
+{
+    public class EventController : Controller
+    {
+        // List of all events
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Event()
+        {
+            return View();
+        }
+    }
+}
