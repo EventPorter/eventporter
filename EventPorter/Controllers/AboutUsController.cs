9b949d8f336dbd5361215f4dc83f96eb33b9e6a8
Aneres <serena0aneres@gmail.com>
2017-09-17 23:58:12 +0100

    About Us Controller and View. Still gotta do actual  proper content and styling

---
+++ EventPorter/Controllers/AboutUsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventPorter.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        // GET: AboutUs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
