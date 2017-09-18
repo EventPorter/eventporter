3f675e3f5e4120b1ac5b90bd24ba9f3b8638dd57
lowflying <lowflyingnow@gmail.com>
2017-09-18 13:23:04 +0100

    Put all create table statemtns into one script

---
+++ SQL_Scripts/CreateAllTables.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE [dbo].[Event] (
+    [ID]               INT             IDENTITY (1, 1) NOT NULL,
+    [CreatorUserName]  NVARCHAR (20)   NOT NULL,
+    [Title]            NVARCHAR (255)  NOT NULL,
+    [Description]      NVARCHAR (1500) NOT NULL,
+    [StartDateAndTime] DATETIME        NOT NULL,
+    [EndDateAndTime]   DATETIME        NULL,
+    [Thumbnail]        NVARCHAR (255)  NULL,
+    [Price]            DECIMAL (18)    NULL,
+    [Longitude]        FLOAT (53)      NULL,
+    [Latitude]         FLOAT (53)      NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+GO
+CREATE TABLE [dbo].[User] (
+    [ID]               INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]        NVARCHAR (55)  NOT NULL,
+    [LastName]         NVARCHAR (55)  NOT NULL,
+    [DOB]              DATETIME       NOT NULL,
+    [Email]            NVARCHAR (55)  NOT NULL,
+    [UserName]         NVARCHAR (20)  NOT NULL UNIQUE,
+    [Password]         NVARCHAR (255) NOT NULL,
+    [UserType]         INT            NOT NULL,
+    [RegistrationDate] DATETIME       NOT NULL,
+    [Location]         NVARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+GO
+CREATE TABLE [dbo].[EventImage] (
+    [EventID]  INT           NOT NULL,
+    [FilePath] VARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_EventImage] PRIMARY KEY CLUSTERED ([EventID] ASC, [FilePath] ASC),
+    CONSTRAINT [FK_Event] FOREIGN KEY ([EventID]) REFERENCES [dbo].[Event] ([ID])
+);
+
+
+
+
